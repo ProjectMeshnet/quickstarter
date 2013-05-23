9352a09214e7acda1410d730735bd724fb05a50a
Luke Evers <me@lukevers.com>
2013-05-23 16:59:37 -0400

    Working on getting btc to auto update

--- config.php
+++ config.php
@@ -1,7 +1,12 @@
 <?php
 
-	$backers = 9;
-	$btc = 3.5;
+	$json = file_getcontents("http://blockchain.info/address/1LQGG3P73ioV2e9jo8uzyCjnEJYxYpkUMR?format=json");
+	$data = json_decode($json, TRUE);
+
+	$backers = 6 + $data[2];
+
+	$btc = $data[3].substring(0)+'.'+$data[3].substring(1);;
+
 	$stripe = 555;
 	$goal = 3500;
 
