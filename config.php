36b282a03c66c89a5869010b747fa48f6ef0a0fa
Luke Evers <me@lukevers.com>
2013-05-23 19:50:21 -0400

    Updating stripe

--- config.php
+++ config.php
@@ -3,13 +3,13 @@
         $json = file_get_contents("http://blockchain.info/address/1LQGG3P73ioV2e9jo8uzyCjnEJYxYpkUMR?format=json");
         $data = json_decode($json, TRUE);
 
-        $stripebackers = 7;
+        $stripebackers = 8;
         $backers = $data["n_tx"] + $stripebackers;
 
         $btc = $data["final_balance"];
         $btc_final = $btc/100000000;
 
-        $stripe = 680;
+        $stripe = 805;
         $goal = 3500;
 
         $ratejson = file_get_contents("http://blockchain.info/ticker");
