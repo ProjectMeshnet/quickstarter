8a8aaa19594596e1fc8e3af73e4e41ee0811fa05
Luke Evers <me@lukevers.com>
2013-05-23 20:16:54 -0400

    Stripe

--- config.php
+++ config.php
@@ -1,18 +1,15 @@
 <?php
-	$date = strtotime("July 30, 2013 12:00 PM");
-	$remaining = $date - time(); // in seconds
-	$days_remaining = floor($remaining / 86400);
 
         $json = file_get_contents("http://blockchain.info/address/1LQGG3P73ioV2e9jo8uzyCjnEJYxYpkUMR?format=json");
         $data = json_decode($json, TRUE);
 
-        $stripebackers = 8;
+        $stripebackers = 9;
         $backers = $data["n_tx"] + $stripebackers;
 
         $btc = $data["final_balance"];
         $btc_final = $btc/100000000;
 
-        $stripe = 805;
+        $stripe = 930;
         $goal = 3500;
 
         $ratejson = file_get_contents("http://blockchain.info/ticker");
