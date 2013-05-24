6516d3de74662b00ef02672d6702fe90d9541382
Luke Evers <me@lukevers.com>
2013-05-24 15:19:02 -0400

    Added the js file locally

--- preorder/checkout/index.php
+++ preorder/checkout/index.php
@@ -6,7 +6,7 @@
     <link rel="shortcut icon" href="../../img/favicon.png">
     <script type="text/javascript" src="../../js/jquery.js"></script>
     <script type="text/javascript" src="../../js/common.js"></script>
-    <script type="text/javascript" src="https://blockchain.info//Resources/wallet/pay-now-button.js"></script>
+    <script type="text/javascript" src="../../js/checkout.js"></script>
 </head>
 <body>
 	<div class="header"></div>
@@ -18,26 +18,24 @@
             <a href="https://donate.meshwith.me" class="blue_button pay">
             	Credit card
             </a>
-		<div style="font-size:16px;margin:0 auto;width:300px" class="blockchain-btn"
-     data-address="1LQGG3P73ioV2e9jo8uzyCjnEJYxYpkUMR"
-     data-shared="false">
-    <div class="blockchain stage-begin">
-        <img src="https://blockchain.info//Resources/buttons/donate_64.png"/>
-    </div>
-    <div class="blockchain stage-loading" style="text-align:center">
-        <img src="https://blockchain.info//Resources/loading-large.gif"/>
-    </div>
-    <div class="blockchain stage-ready">
-         <p align="center">Please Donate To Bitcoin Address: <b>[[address]]</b></p>
-         <p align="center" class="qr-code"></p>
-    </div>
-    <div class="blockchain stage-paid">
-         Donation of <b>[[value]] BTC</b> Received. Thank You.
-    </div>
-    <div class="blockchain stage-error">
-        <font color="red">[[error]]</font>
-    </div>
-</div>
+		<div style="font-size:16px;margin:0 auto;width:300px" class="blockchain-btn" data-address="1LQGG3P73ioV2e9jo8uzyCjnEJYxYpkUMR" data-shared="false">
+		   <div class="blockchain stage-begin">
+        	    <img src="https://blockchain.info//Resources/buttons/donate_64.png"/>
+    		   </div>
+   		 <div class="blockchain stage-loading" style="text-align:center">
+     		   <img src="https://blockchain.info//Resources/loading-large.gif"/>
+   		 </div>
+  		  <div class="blockchain stage-ready">
+     		    <p align="center">Please Donate To Bitcoin Address: <b>[[address]]</b></p>
+     		    <p align="center" class="qr-code"></p>
+   		 </div>
+ 		   <div class="blockchain stage-paid">
+      		   Donation of <b>[[value]] BTC</b> Received. Thank You.
+   		 </div>
+    		<div class="blockchain stage-error">
+      		  <font color="red">[[error]]</font>
+    		</div>
+		</div>
         </p>
     </div>
     <div class="footerpay">
