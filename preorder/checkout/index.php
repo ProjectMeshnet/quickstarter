3a1123e3c715e0729eb5a8492a8f341f459408c3
Luke Evers <me@lukevers.com>
2013-05-24 15:14:39 -0400

    Adding the donate now button for btc

---
+++ preorder/checkout/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Expand Meshnet</title>
+    <link rel="stylesheet" href="../../css/main.css">
+    <link rel="shortcut icon" href="../../img/favicon.png">
+    <script type="text/javascript" src="../../js/jquery.js"></script>
+    <script type="text/javascript" src="../../js/common.js"></script>
+    <script type="text/javascript" src="https://blockchain.info//Resources/wallet/pay-now-button.js"></script>
+</head>
+<body>
+	<div class="header"></div>
+    <div class="main">
+    	<h1>Checkout</h1>
+        <p>
+        	There are two ways to donate to the project.
+            <br/>Upon donating with Bitcoin, please email us at <a href="mailto:support@meshwith.me">support@meshwith.me</a>.
+            <a href="https://donate.meshwith.me" class="blue_button pay">
+            	Credit card
+            </a>
+		<div style="font-size:16px;margin:0 auto;width:300px" class="blockchain-btn"
+     data-address="1LQGG3P73ioV2e9jo8uzyCjnEJYxYpkUMR"
+     data-shared="false">
+    <div class="blockchain stage-begin">
+        <img src="https://blockchain.info//Resources/buttons/donate_64.png"/>
+    </div>
+    <div class="blockchain stage-loading" style="text-align:center">
+        <img src="https://blockchain.info//Resources/loading-large.gif"/>
+    </div>
+    <div class="blockchain stage-ready">
+         <p align="center">Please Donate To Bitcoin Address: <b>[[address]]</b></p>
+         <p align="center" class="qr-code"></p>
+    </div>
+    <div class="blockchain stage-paid">
+         Donation of <b>[[value]] BTC</b> Received. Thank You.
+    </div>
+    <div class="blockchain stage-error">
+        <font color="red">[[error]]</font>
+    </div>
+</div>
+        </p>
+    </div>
+    <div class="footerpay">
+    	&copy; 2013 Project Meshnet <span class="x">&nbsp;</span>
+    </div>
+</body>
+</html>
