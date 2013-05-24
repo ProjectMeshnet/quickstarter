0efb57e3078e59fdeb96c271857be4fdf48bcbbb
Luke Evers <me@lukevers.com>
2013-05-23 20:13:32 -0400

    Adding more to stripe.
    Working on integration soon.

--- config.php
+++ config.php
@@ -3,13 +3,13 @@
         $json = file_get_contents("http://blockchain.info/address/1LQGG3P73ioV2e9jo8uzyCjnEJYxYpkUMR?format=json");
         $data = json_decode($json, TRUE);
 
-        $stripebackers = 8;
+        $stripebackers = 9;
         $backers = $data["n_tx"] + $stripebackers;
 
         $btc = $data["final_balance"];
         $btc_final = $btc/100000000;
 
-        $stripe = 805;
+        $stripe = 930;
         $goal = 3500;
 
         $ratejson = file_get_contents("http://blockchain.info/ticker");
